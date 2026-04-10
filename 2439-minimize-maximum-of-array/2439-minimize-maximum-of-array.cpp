b87276bf7806c2e3fb59ca4fd29627e9d32a7284
yashaswi singh <164437644+Yashaswisingh3458@users.noreply.github.com>
2026-04-11 01:23:33 +0530

    Time: 0 ms (100%), Space: 75.1 MB (44.62%) - LeetHub

---
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimizeArrayValue(vector<int>& nums) {
+        long long sum = 0;
+        int ans = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            int avg = (sum + i) / (i + 1);
+            ans = max(ans, avg);
+        }
+        return ans;
+    }
+};
