3d90cfba2f95bd151216acae03731e9c2d098f3a
yashaswi singh <164437644+Yashaswisingh3458@users.noreply.github.com>
2026-06-29 01:39:06 +0530

    Time: 5 ms (79.27%), Space: 55.3 MB (36.97%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -3,16 +3,7 @@ public:
     int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
         int y=0;
         sort(arr.begin(),arr.end());
-        int i =0;
-        while(i<arr.size() && arr[i]!=1){
-            i+=1;
-        }
-        if(i<arr.size()){
-            swap(arr[0],arr[i]);
-        }
-        else{
-            arr[0]=1;
-        }
+        arr[0]=1;
         int s=0;
         for(int i =0;i<arr.size();i++){
             if(arr[i]>=s){
